73aac385d4e13639732d8d532e45d16914f8a8d0
Antoine Comerre <antoine.comerre@epfl.ch>
2024-05-15 12:06:48 +0200

    là

--- exercice0.py
+++ exercice0.py
@@ -5,3 +5,4 @@ for i in range(2):
                 for m in range(2):
                     if ((not i or j) and (i or j or not k) and (k or l) and (not j or not k or l) and (not l or not m) and (not k or not l or m)):
                         print(i, j, k, l, m)
+
